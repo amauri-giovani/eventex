0be9ff8a8338e1bf091a52f820fb0133017c3cee
Amauri Rossetti Giovani <amauri.giovani@gmail.com>
2024-12-10 17:55:59 -0300

    Changed github account

---
+++ eventex/subscriptions/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from eventex.subscriptions.forms import SubscriptionForm
+
+
+def subscribe(request):
+    if request.method == 'POST':
+        return HttpResponseRedirect('/inscricao/')
+    else:
+        context = {'form': SubscriptionForm()}
+        return render(request, 'subscription_form.html', context)
